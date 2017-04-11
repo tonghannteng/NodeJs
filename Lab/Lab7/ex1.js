79a1178e1c1261bade2617018971d390ba1f9d01
Tonghann Teng <tengtonghann@gmail.com>
2017-04-11 14:25:11 -0500

    edit

--- Lab/Lab7/ex1.js
+++ Lab/Lab7/ex1.js
@@ -1,34 +1,10 @@
-var express = require('express');
-var app = express();
-var MongoClient = require('mongodb').MongoClient;
+var book = {
 
-app.listen(8000,()=>console.log('listen port 8000'));
-
-app.get('/', function (req, res) {
-    MongoClient.connect('mongodb://127.0.0.1:27017/lab07',(err,db)=>{
-        if(err) throw err;
-        db.collection('homework7').findOne({},(err,doc)=>{
-            res.send(decrypt(doc.message));
-        });
-    });
-    
-});
-
-function decrypt(encrypted){
-    const crypto = require('crypto');
-    const decipher = crypto.createDecipher('aes256', 'asaadsaad');
-
-    let decrypted = '';
-    decipher.on('readable', () => {
-    const data = decipher.read();
-    if (data)
-        decrypted += data.toString('utf8');
-    });
-    decipher.on('end', () => {
-        console.log(decrypted);
-    });
-
-    decipher.write(encrypted, 'hex');
-    decipher.end();
-    return decrypted;
+    _id:objectId(),
+    name:'BookName',
+    ISBN:'1234',
+    author:'AuthorName',
+    keywords:['key1', 'key2'],
+    borrowe:'name',
+    dueDate:'01/01/2017'
 }
